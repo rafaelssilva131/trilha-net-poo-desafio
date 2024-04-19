4492e175aa80d21347f03b29a51025a168a69594
Rafael Souza <rafael.ssilva131@gmail.com>
2024-04-19 01:41:32 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone nokia = new Nokia("9123456789", "Modelo Nokia", "987654321", 124);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Smartphone iphone = new Iphone("9123456789", "Modelo Iphone", "123456789", 64);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("WhatsApp");
+        }
+    }
+}
